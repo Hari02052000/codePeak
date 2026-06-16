6c8510f30f3d05e62619e8e4ac8115f3d32a13b8
Arunpk1 <arunpkong@gmail.com>
2026-06-16 21:37:49 +0530

    new changes

--- backend/problem-service/src/entities/test.case.ts
+++ backend/problem-service/src/entities/test.case.ts
@@ -1,1 +1,42 @@
-export class TestCase {}
+
+import { testcasetype } from "./types/testcase";
+export class TestCase {
+
+
+   private constructor(private props: testcasetype) {
+   
+
+  }
+   setId(id: string) {
+    this.props.id = id;
+  }
+  
+   setInput(input: string) {
+    
+    this.props.input = input;
+  }
+    setOutput(output: string) {
+    this.props  .output = output;
+  }
+    setExplanation(explanation: string) {
+    this.props.explanation = explanation;
+  }
+   setProblemId(problemId: string) {
+    this.props.problemId = problemId;
+  }
+  static create(props:  Omit< testcasetype, "id">) {
+    
+    if ( !props.input || !props.output || !props.problemId) {
+      throw new Error("Missing required fields");
+
+    }
+
+    return new TestCase({id:"",...props});
+
+  }
+  static recreate (props: testcasetype) {
+    return new TestCase(props);
+  }
+
+
+}
